caa21a17c4494176932808ccc69435775cda760a
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-04-04 19:55:17 -0700

    state test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,33 @@
-import * as fs from "node:fs";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
 
-const filePath = "count.txt";
-
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, "utf-8").catch(() => "0")
-  );
-}
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "~/components/ui/alert-dialog";
+import { getCookie, getHeaders, setCookie } from "@tanstack/react-start/server";
 
 const getCount = createServerFn({
   method: "GET",
 }).handler(() => {
-  return readCount();
+  return parseInt(getCookie("marker-count") ?? "0");
 });
 
 const updateCount = createServerFn({ method: "POST" })
   .validator((d: number) => d)
   .handler(async ({ data }) => {
-    const count = await readCount();
-    await fs.promises.writeFile(filePath, `${count + data}`);
+    const count = await getCount();
+    const newCount = count + data;
+    setCookie("marker-count", newCount.toString());
+
+    return newCount;
   });
 
 export const Route = createFileRoute("/")({
@@ -29,19 +36,39 @@ export const Route = createFileRoute("/")({
 });
 
 function Home() {
-  const router = useRouter();
   const state = Route.useLoaderData();
+  const router = useRouter();
 
   return (
-    <button
-      type="button"
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate();
-        });
-      }}
-    >
-      Add 1 to {state}?
-    </button>
+    <main className="flex h-screen flex-col items-center justify-center">
+      <div>You have made {state} markers</div>
+      <AlertDialog>
+        <AlertDialogTrigger className="bg-primary text-primary-foreground rounded-md px-4 py-2">
+          Make a Marker
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Are you ready to mark your record?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              This is still experimental
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={async () => {
+                updateCount({ data: 1 }).then(() => {
+                  router.invalidate();
+                });
+              }}
+            >
+              Marker time!!
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </main>
   );
 }
