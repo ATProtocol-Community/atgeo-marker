588a8ae4ebb96908d9c8e852722732014624bce4
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-06-03 16:31:28 -0500

    redo aliases to resolve properly

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from "@tanstack/react-start/config";
+import path from "path";
 import tsConfigPaths from "vite-tsconfig-paths";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   tsr: {
@@ -21,5 +27,12 @@ export default defineConfig({
         projects: ["./tsconfig.json"],
       }),
     ],
+    resolve: {
+      alias: {
+        "~/generated": path.resolve(__dirname, "generated"),
+        "~/lexicons": path.resolve(__dirname, "lexicons"),
+        "~": path.resolve(__dirname, "src"),
+      },
+    },
   },
 });
