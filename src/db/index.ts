1d4dce3b967b2db5c89ddc5ecc620278da02790b
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-06-07 17:08:06 -0700

    more kysely config

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import SQLite from "better-sqlite3";
 import { Kysely, SqliteDialect } from "kysely";
 
 const dialect = new SqliteDialect({
-  database: new SQLite("db.sqlite"),
+  database: new SQLite(`${process.env.DB_PATH}`),
 });
 
 export const db = new Kysely<Database>({
