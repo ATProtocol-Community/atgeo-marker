6ef074b0cab39250890b8576238f6d88c857c144
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-06-07 17:05:41 -0700

    kysely config

--- kysely.config.ts
+++ kysely.config.ts
@@ -4,7 +4,7 @@ import SQLite from "better-sqlite3";
 export default defineConfig({
   dialect: "better-sqlite3",
   dialectConfig: {
-    database: new SQLite("db.sqlite"),
+    database: new SQLite(`${process.env.DB_PATH}/database.db`),
   },
   migrations: {
     migrationFolder: "src/db/migrations",
